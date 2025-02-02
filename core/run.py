fa5c1b218166bc530d5ef1a032d71db3eed76596
KhazaeiAmir110 <khazaei.amir110@gmail.com>
2025-02-02 11:55:20 +0330

    change data

--- core/run.py
+++ core/run.py
@@ -5,9 +5,14 @@ from core.test import PhonePricePredictor
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 if __name__ == "__main__":
-    source_csv_path = "../input_files/source.csv"
-    result_csv_path = "../output_files/result.csv"
-    predictor = PhonePricePredictor(source_csv_path, result_csv_path)
+    market_posts_path = "../input_files/db_data/market_posts.csv"
+    phone_main_path = "../input_files/db_data/phone_main.csv"
+    phone_models_path = "../input_files/db_data/phone_models.csv"
+    phone_models2_path = "../input_files/db_data/phone_models2.csv"
+
+    result_csv_path = "../output_files/output_phone.csv"
+
+    predictor = PhonePricePredictor(market_posts_path, result_csv_path)
     is_valid, mean_error, std_error, process_time = predictor.run()
     if is_valid:
         print(f"Process completed successfully in {process_time} seconds.")
--- core/test.py
+++ core/test.py
@@ -127,3 +127,14 @@ class PhonePricePredictor:
         except CustomError as e:
             logging.error(f"Process failed: {e}")
             return False, 0, 0, 0
+
+
+if __name__ == "__main__":
+    source_csv_path = "../input_files/source.csv"
+    result_csv_path = "../output_files/result.csv"
+    predictor = PhonePricePredictor(source_csv_path, result_csv_path)
+    is_valid, mean_error, std_error, process_time = predictor.run()
+    if is_valid:
+        print(f"Process completed successfully in {process_time} seconds.")
+    else:
+        print("Process failed.")
